8145d13b8d9140b504c81a00f0a9e27d94cad23f
Vinicius Charles <viniciuscharles3101@gmail.com>
2024-12-18 14:47:13 -0300

    Projeto iniciado

---
+++ src/main/java/br/com/senac/senactds_project_pi2/SenacTDS_Project_PI2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.senac.senactds_project_pi2;
+
+/**
+ *
+ * @author Vinicius charles
+ */
+public class SenacTDS_Project_PI2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

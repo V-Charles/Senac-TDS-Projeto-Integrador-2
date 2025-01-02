6f69fe50b2c6734413dac242fdabd87e7c28b622
Vinicius Charles <viniciuscharles3101@gmail.com>
2025-01-02 14:41:12 -0300

    Criadas as classes FornecedorDTO e FornecedorDAO

---
+++ src/main/java/entidades/FornecedorDTO.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+/**
+ *
+ * @author Vinicius charles
+ */
+
+@Entity
+public class FornecedorDTO {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nomeFornecedor;
+    private String cnpj;
+    private String telefone;
+    private String email;
+    private boolean ativo = true;
+    
+    public FornecedorDTO(){
+    }
+
+    public FornecedorDTO(int id, String nomeFornecedor, String cnpj, String telefone, String email, boolean ativo) {
+        this.id = id;
+        this.nomeFornecedor = nomeFornecedor;
+        this.cnpj = cnpj;
+        this.telefone = telefone;
+        this.email = email;
+        this.ativo = ativo;
+    }
+    
+    /**getters e setters
+     * 
+     * @return 
+     */
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNomeFornecedor() {
+        return nomeFornecedor;
+    }
+
+    public void setNomeFornecedor(String nomeFornecedor) {
+        this.nomeFornecedor = nomeFornecedor;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+    
+    @Override
+    public String toString(){
+        return this.getNomeFornecedor();
+    }
+}

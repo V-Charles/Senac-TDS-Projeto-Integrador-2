7aeb9fb921450e48bb11f87c7a0528f705e5d77e
Vinicius Charles <viniciuscharles3101@gmail.com>
2025-01-05 15:17:26 -0300

    Implementado método listar() que retorna uma lista de objetos Produto com base em um filtro opcional de nome

--- src/main/java/entidades/ProdutoDAO.java
+++ src/main/java/entidades/ProdutoDAO.java
@@ -4,10 +4,29 @@
  */
 package entidades;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import java.util.List;
+import utilitarios.JPAUtil;
+
 /**
  *
  * @author Vinicius charles
  */
 public class ProdutoDAO {
     
+    public List<Produto> listar(String filtroNome){
+        EntityManager em = JPAUtil.getEntityManager();
+        List produtos = null;
+        try {
+            String textoQuery = "SELECT p FROM Produto p " + 
+                    "WHERE (:nomeProduto IS NULL OR p.nomeProduto LIKE :nomeProduto)";
+            Query consulta = em.createQuery(textoQuery);
+            consulta.setParameter("nomeProduto", filtroNome.isEmpty() ? null : "%" + filtroNome + "%");
+            produtos = consulta.getResultList();
+        } finally {
+            JPAUtil.closeEntityManager();
+        }
+        return produtos;
+    }
 }

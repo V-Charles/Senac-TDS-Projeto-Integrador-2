600e7728661c840bc3822a951a8feafabb5f0937
Vinicius Charles <viniciuscharles3101@gmail.com>
2025-01-05 14:47:45 -0300

    Criadas as classes Produto e ProdutoDAO

---
+++ src/main/java/entidades/Produto.java
@@ -0,0 +1,111 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Vinicius charles
+ */
+
+@Entity
+public class Produto {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    private String nomeProduto;
+    private int quantidade;
+    private String categoria;
+    
+    @Column(name = "data")
+    private LocalDate data;
+    
+    @ManyToOne
+    @JoinColumn(name = "fornecedor_id")
+    private Fornecedor fornecedor;
+    
+    public Produto(){
+    }
+
+    public Produto(int id, String nomeProduto, int quantidade, String categoria, LocalDate data, Fornecedor fornecedor) {
+        this.id = id;
+        this.nomeProduto = nomeProduto;
+        this.quantidade = quantidade;
+        this.categoria = categoria;
+        this.data = data;
+        this.fornecedor = fornecedor;
+    }
+    
+    /**getters e setters
+     * @return 
+     */
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public Fornecedor getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" + "id=" + id + 
+                ", nomeProduto=" + nomeProduto + 
+                ", quantidade=" + quantidade + 
+                ", categoria=" + categoria + 
+                ", data=" + data + 
+                ", fornecedor=" + fornecedor + '}';
+    }
+}
